bfe9a7584aedeca85b8f681a5d1c9d62de2ea315
p-jean <e1@walkingalong.com>
2025-05-27 15:46:51 -0400

    Fix noise and continuous repeats for Arduino-base media-players with home assistant TTS starting in 2025.4.0 (#19)

--- src/Audio.h
+++ src/Audio.h
@@ -137,7 +137,10 @@ public:
     uint32_t getReadPos();                      // read position relative to the beginning
     void     resetBuffer();                     // restore defaults
     bool     havePSRAM() { return m_f_psram; };
-
+    void     setComplete() { m_complete = true; }; // indicates that the buffer contains the last data of a source
+    bool     isComplete() { return m_complete; }; // returns true if the buffer contains the last data of a source
+    bool     isEmpty() { return bufferFilled() == 0; }; // returns true if the buffer is empty
+    bool     isPlayable() { return isComplete() || (bufferFilled() >= m_maxBlockSize); }; // returns true if the buffer is either complete or has enough data to play
 protected:
     size_t   m_buffSizePSRAM    = 300000;   // most webstreams limit the advance to 100...300Kbytes
     size_t   m_buffSizeRAM      = 1600 * 5;
@@ -155,6 +158,7 @@ protected:
     bool     m_f_start          = true;
     bool     m_f_init           = false;
     bool     m_f_psram          = false;    // PSRAM is available (and used...)
+    bool     m_complete         = false;    // true if the buffer contains the last data from a source.    
 };
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -278,6 +282,7 @@ private:
 
 //+++ W E B S T R E A M  -  H E L P   F U N C T I O N S +++
     uint16_t readMetadata(uint16_t b, bool first = false);
+    bool     stripCRLF(int prevChar = 0);
     size_t   chunkedDataTransfer(uint8_t* bytes);
     bool     readID3V1Tag();
     void     slowStreamDetection(uint32_t inBuffFilled, uint32_t maxFrameSize);
